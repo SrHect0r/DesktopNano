0715a01e90113a6b9f558da58d6b1139bc4e0a8c
SrHect0r <hmudarral2425@politecnics.barcelona>
2025-10-16 19:46:28 +0200

    Commit all

--- DesktopNano/Form1.Designer.cs
+++ DesktopNano/Form1.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            groupBox1 = new GroupBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(316, 148);
+            label1.Name = "label1";
+            label1.Size = new Size(123, 15);
+            label1.TabIndex = 0;
+            label1.Text = "El Daniil es un grande!";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(148, 218);
+            label2.Name = "label2";
+            label2.Size = new Size(524, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Tienes que instalarte unos paquetes raro, porque no tienenes nada en el \"Cuadro de herramientas\"";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(316, 184);
+            label3.Name = "label3";
+            label3.Size = new Size(82, 15);
+            label3.TabIndex = 2;
+            label3.Text = "que rico papa!";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(79, 33);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(200, 100);
+            groupBox1.TabIndex = 3;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "pepe";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private GroupBox groupBox1;
     }
 }
